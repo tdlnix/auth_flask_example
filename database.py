a3897f52e862be6a6ea8ff96f4bac6c36e00ae04
tdlnix <salnikovtd@ipran.ru>
2022-08-10 11:30:20 +0300

    Base sqlalchemy db realisation

--- database.py
+++ database.py
@@ -1,4 +1,10 @@
 from abc import ABC, abstractmethod
+import os
+
+from sqlalchemy import Column
+from sqlalchemy import Integer, String
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
 
 
 class Database(ABC):
@@ -18,3 +24,34 @@ class Database(ABC):
     @abstractmethod  
     def delete_user(self, username):
         pass
+
+
+class SQLAlchemyDB(Database):
+    def __init__(self, path):
+        path = os.path.join("sqlite:///", path)
+        self.engine = create_engine(path, echo=True, future=True)
+        self.Base = declarative_base()
+        self.User = self.create_user_table()
+        self.Base.metadata.create_all(self.engine)
+
+    def create_user_table(self):
+        class User(self.Base):
+            __tablename__ = 'user_account'
+            id = Column(Integer, primary_key=True)
+            name = Column(String(20), nullable=False, unique=True)
+            password = Column(String(80), nullable=False)
+        return User
+
+    def add_user(self, username, password):
+        pass
+
+    def get_user(self, username):
+        pass
+
+    @abstractmethod  
+    def delete_user(self, username):
+        pass
+
+
+if __name__ == "__main__":
+    db = SQLAlchemyDB("database.db")
